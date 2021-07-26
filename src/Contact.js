a8079ef8db88a34755fdb3b484bc0fb2586b876d
Lenar <53114683+LenarXLA@users.noreply.github.com>
2021-07-26 16:03:24 +0300

    init: SPA app

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Есть вопросы?</h2>
+                <p>
+                    Обращайтесь по адресу: ...
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
